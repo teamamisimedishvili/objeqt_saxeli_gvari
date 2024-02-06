e29d8add9f312eb355c444a9fcc5a85cfbf64880
teamamisimedishvili <127042618+teamamisimedishvili@users.noreply.github.com>
2024-02-06 04:50:05 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var obieqti = {
+    name : "თეა",
+    lastname : "მამისიმედიშვილი",
+    age : 46,
+    height : 163
+};
+
+document.write(obieqti.name + ",");
+
+document.write(obieqti.lastname + ",");
+document.write(obieqti.age + "," );
+document.write(obieqti.height);
